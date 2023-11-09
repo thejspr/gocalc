7164fb38f333a93984e235d41ffe1760c1d1c54a
Jesper Kjeldgaard <thejspr@gmail.com>
2023-11-09 16:08:30 +0100

    multiline calc

--- main.go
+++ main.go
@@ -3,14 +3,40 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
+
 	"github.com/Pramod-Devireddy/go-exprtk"
 )
 
 func main() {
-	calculate(flag.Arg(0))
+	flag.Parse()
+	// for _, result := range strings.Split(flag.Arg(0), ",") {
+	// 	fmt.Println(result)
+	// }
+	results, err := Multicalc(strings.Split(flag.Arg(0), ","))
+
+	if err != nil {
+		return
+	}
+
+	for _, result := range results {
+		fmt.Println(result)
+	}
 }
 
-func calculate(expression string) (float64, error) {
+func Multicalc(expressions []string) ([]float64, error) {
+	var results []float64
+	for _, expression := range expressions {
+		result, err := Calculate(expression)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+	return results, nil
+}
+
+func Calculate(expression string) (float64, error) {
 	exprtkObj := exprtk.NewExprtk()
 	defer exprtkObj.Delete()
 
@@ -23,7 +49,7 @@ func calculate(expression string) (float64, error) {
 	}
 
 	var result = exprtkObj.GetEvaluatedValue()
-	fmt.Println(result)
+	// fmt.Println(result)
 
 	return result, err
 }
--- main_test.go
+++ main_test.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"math"
+	"reflect"
 	"testing"
 )
 
 const float64EqualityThreshold = 1e-9
-
-func validateExpression(t *testing.T, expression string, expected float64) {
-	result, err := calculate(expression)
-	if !(math.Abs(result-expected) <= float64EqualityThreshold) || err != nil {
-		t.Fatalf("Expected %f, got %f", expected, result)
-	}
-}
 
 func TestAddExpression(t *testing.T) {
 	validateExpression(t, "1 + 1", 2)
@@ -24,4 +18,28 @@ func TestDivideExpression(t *testing.T) {
 
 func TestMultiplyExpression(t *testing.T) {
 	validateExpression(t, "10 * 10", 100)
+}
+
+func TestMultiLineExpression(t *testing.T) {
+	validateExpression(t, "10 * 10\n", 100)
+}
+
+// multiple expressions
+
+func TestMulticalc(t *testing.T) {
+	var expected = []float64{2, 5}
+	result, err := Multicalc([]string{"1 + 1", "10 / 2"})
+
+	if !reflect.DeepEqual(result, expected) || err != nil {
+		t.Fatal(err)
+	}
+}
+
+// helpers
+
+func validateExpression(t *testing.T, expression string, expected float64) {
+	result, err := Calculate(expression)
+	if !(math.Abs(result-expected) <= float64EqualityThreshold) || err != nil {
+		t.Fatalf("Expected %f, got %f", expected, result)
+	}
 }
